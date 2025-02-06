076a8223252eae7ab0ebaae954588bfa6aa91d98
Alvar Sirlin <alvar@alvarsirlin.com>
2025-02-06 10:19:13 -0500

    solve server and htmx error

--- api/server.js
+++ api/server.js
@@ -1,15 +1,37 @@
 import Fastify from 'fastify';
 import pg from 'pg';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fastifyStatic from '@fastify/static';
 
 dotenv.config();
-const fastify = Fastify();
 const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 pool.connect()
-  .then(() => console.log("✅ Database connected"))
-  .catch(err => console.error("❌ Database connection error:", err));
+  .then(() => console.log("Database connected"))
+  .catch(err => console.error("Database connection error:", err));
 
+const fastify = Fastify({ logger: true });
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(`Serving static files from: ${path.join(__dirname, 'public')}`);
+
+fastify.register(fastifyStatic, {
+  root: path.join(__dirname, '../public'),
+  prefix: '/', // optional: default '/'
+});
+
+fastify.listen({ port: PORT, host: '0.0.0.0' }, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
 
 fastify.get('/api/expenses', async (_, reply) => {
   const { rows } = await pool.query('SELECT * FROM transactions ORDER BY date DESC');
@@ -36,5 +58,15 @@ fastify.get('/api/debug-env', async (_, reply) => {
 });
 
 fastify.get('/', async (_, reply) => {
-  reply.send({ message: "Expense Tracker API is running" });
+  return reply.sendFile('index.html');
 });
+
+fastify.setNotFoundHandler((request, reply) => {
+  reply.sendFile('index.html', (err) => {
+    if (err) {
+      reply.status(500).send('Error serving index.html');
+    }
+  });
+});
+
+
